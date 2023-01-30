59dfd0d4210f955ae6d9fccd4e0ccf252e738eaf
Selim <a.s.k-2003@mail.ru>
2023-01-30 18:40:01 +0400

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonqirmizi = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBoxyeniad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonqirmizi
+            // 
+            this.buttonqirmizi.BackColor = System.Drawing.Color.Red;
+            this.buttonqirmizi.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonqirmizi.Location = new System.Drawing.Point(257, 47);
+            this.buttonqirmizi.Name = "buttonqirmizi";
+            this.buttonqirmizi.Size = new System.Drawing.Size(145, 78);
+            this.buttonqirmizi.TabIndex = 0;
+            this.buttonqirmizi.Text = "Qırmızı";
+            this.buttonqirmizi.UseVisualStyleBackColor = false;
+            this.buttonqirmizi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(612, 47);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 77);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Yaşıl";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(455, 256);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 72);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "SƏLİM";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Purple;
+            this.button6.Location = new System.Drawing.Point(612, 179);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(142, 78);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Bənövşəyi";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(455, 110);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 72);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "Ağ";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Yellow;
+            this.button12.Location = new System.Drawing.Point(612, 311);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(142, 78);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "Sarı";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(106, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyad";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(257, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 78);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Qara";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(257, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 78);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Mavi";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 237);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 169);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Daxil et";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBoxyeniad
+            // 
+            this.textBoxyeniad.Location = new System.Drawing.Point(12, 179);
+            this.textBoxyeniad.Name = "textBoxyeniad";
+            this.textBoxyeniad.Size = new System.Drawing.Size(230, 23);
+            this.textBoxyeniad.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxyeniad);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.buttonqirmizi);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonqirmizi;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBoxyeniad;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BackColor=Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var name = textBox1.Text;
+            var surname=textBox2.Text;
+
+            MessageBox.Show(name + " " + surname);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Green;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Violet;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Yellow;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Blue;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            BackColor = Color.Black;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sira = listBox1.SelectedIndex;
+            listBox1.Items.RemoveAt(sira);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBoxyeniad.Text != "")
+            {
+            
+            string vergul = ",";
+            bool netice = textBoxyeniad.Text.Contains(vergul);
+
+                if (netice)
+                {
+                    listBox1.Items.Add(textBoxyeniad.Text.Replace(vergul, " "));
+                }
+                else
+                {
+                    listBox1.Items.Add(textBoxyeniad.Text);
+                    textBoxyeniad.Text = "";
+                    textBoxyeniad.Focus();
+                }
+
+            }
+           
+        }
+    }
+}
